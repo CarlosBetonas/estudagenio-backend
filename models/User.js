981591e681b54026b426cde2407e75afebe85049
none <>
2025-11-27 11:57:39 -0300

    Update User.js

--- models/User.js
+++ models/User.js
@@ -3,16 +3,15 @@ const mongoose = require('mongoose');
 const UserSchema = new mongoose.Schema({
     fullName: { type: String, required: true },
     email: { type: String, required: true, unique: true },
-    password: { type: String, required: true }, // Senha será criptografada
+    password: { type: String, required: true }, 
     birthDate: { type: Date, required: true },
     termsAccepted: { type: Boolean, required: true },
     
-    // Campos do Onboarding (inicialmente vazios)
     preferences: {
-        exams: [String], // Ex: ['ENEM', 'FUVEST']
-        subjects: [String], // Ex: ['Física', 'História']
-        goal: String, // Ex: 'beginner'
-        isPremium: { type: Boolean, default: false }  // 👈 IMPORTANTE
+        exams: [String], 
+        subjects: [String],
+        goal: String, 
+        isPremium: { type: Boolean, default: false }  
     },
     createdAt: { type: Date, default: Date.now }
 });
